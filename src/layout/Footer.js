f85e531c1f3d0b0613c12c358bf88aa255ef4b99
PrasathVjKmr <prasathvjkmr@gmail.com>
2022-12-22 20:45:13 +0530

    layout

---
+++ src/layout/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <>
+      <ul>
+        <li>GitHub</li>
+        <li>LinkedIn</li>
+        <li>Codepen</li>
+        <li>Instagram</li>
+      </ul>
+    </>
+  );
+}
---
+++ src/layout/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <>
+      <ul>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+    </>
+  );
+}
